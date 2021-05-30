f877815f19d8349306387062715a7aec01dcfcfa
Dylan Longlands <d.longlands99@gmail.com>
2021-05-30 23:36:11 +0100

    Convert User from class based to FC

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,121 +1,117 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Repos from '../repos/Repos';
 
-class User extends Component {
-    static propTypes = {
-        getUserDetails: PropTypes.func.isRequired,
-        user: PropTypes.object.isRequired,
-        loading: PropTypes.bool.isRequired,
-        getRepos: PropTypes.func.isRequired,
-        repos: PropTypes.array.isRequired,
-    };
+const User = ({ user, loading, repos, getRepos, getUserDetails, match }) => {
+    useEffect(() => {
+        getUserDetails(match.params.login);
+        getRepos(match.params.login);
+    }, []);
 
-    componentDidMount() {
-        this.props.getUserDetails(this.props.match.params.login);
-        this.props.getRepos(this.props.match.params.login);
-    }
+    const {
+        name,
+        avatar_url,
+        location,
+        bio,
+        blog,
+        login,
+        company,
+        html_url,
+        followers,
+        following,
+        public_repos,
+        public_gists,
+        hireable,
+    } = user;
 
-    render() {
-        const {
-            name,
-            avatar_url,
-            location,
-            bio,
-            blog,
-            login,
-            company,
-            html_url,
-            followers,
-            following,
-            public_repos,
-            public_gists,
-            hireable,
-        } = this.props.user;
-
-        const { loading, user, getRepos, repos } = this.props;
-
-        return (
-            <>
-                {loading && <Spinner />}
-                <Link to='/' className='btn btn-light'>
-                    Back to search
-                </Link>
-                Hireable: {''}
-                {hireable ? (
-                    <i className='fas fa-check text-success' />
-                ) : (
-                    <i className='fas fa-times-circle text-danger' />
-                )}
-                <div className='card grid-2'>
-                    <div className='all-center'>
-                        <img
-                            src={avatar_url}
-                            className='round-img'
-                            style={avatarStyle}
-                        />
-                        <h1>Name: {name}</h1>
-                        <p>Location: {location}</p>
-                    </div>
-                    <div>
-                        {bio && (
-                            <>
-                                <h3>Bio:</h3>
-                                <p>{bio}</p>
-                            </>
-                        )}
-                        <a href={html_url} className='btn btn-dark my-1'>
-                            Visit Github Profile
-                        </a>
-                        <ul>
-                            <li>
-                                {login && (
-                                    <>
-                                        <strong>Username: </strong>
-                                        {login}
-                                    </>
-                                )}
-                            </li>
-                            <li>
-                                {company && (
-                                    <>
-                                        <strong>Company: </strong>
-                                        {company}
-                                    </>
-                                )}
-                            </li>
-                            <li>
-                                {blog && (
-                                    <>
-                                        <strong>Website: </strong>
-                                        {blog}
-                                    </>
-                                )}
-                            </li>
-                        </ul>
-                    </div>
+    return (
+        <Spinner>
+            {loading && <Spinner />}
+            <Link to='/' className='btn btn-light'>
+                Back to search
+            </Link>
+            Hireable: {''}
+            {hireable ? (
+                <i className='fas fa-check text-success' />
+            ) : (
+                <i className='fas fa-times-circle text-danger' />
+            )}
+            <div className='card grid-2'>
+                <div className='all-center'>
+                    <img
+                        src={avatar_url}
+                        className='round-img'
+                        style={avatarStyle}
+                    />
+                    <h1>Name: {name}</h1>
+                    <p>Location: {location}</p>
                 </div>
-                <div className='card text-center'>
-                    <div className='badge badge-primary'>
-                        Followers: {followers}
-                    </div>
-                    <div className='badge badge-success'>
-                        Following: {following}
-                    </div>
-                    <div className='badge badge-light'>
-                        Public Repos: {public_repos}
-                    </div>
-                    <div className='badge badge-dark'>
-                        Public Gists: {public_gists}
-                    </div>
+                <div>
+                    {bio && (
+                        <>
+                            <h3>Bio:</h3>
+                            <p>{bio}</p>
+                        </>
+                    )}
+                    <a href={html_url} className='btn btn-dark my-1'>
+                        Visit Github Profile
+                    </a>
+                    <ul>
+                        <li>
+                            {login && (
+                                <>
+                                    <strong>Username: </strong>
+                                    {login}
+                                </>
+                            )}
+                        </li>
+                        <li>
+                            {company && (
+                                <>
+                                    <strong>Company: </strong>
+                                    {company}
+                                </>
+                            )}
+                        </li>
+                        <li>
+                            {blog && (
+                                <>
+                                    <strong>Website: </strong>
+                                    {blog}
+                                </>
+                            )}
+                        </li>
+                    </ul>
                 </div>
-                <Repos repos={repos} />
-            </>
-        );
-    }
-}
+            </div>
+            <div className='card text-center'>
+                <div className='badge badge-primary'>
+                    Followers: {followers}
+                </div>
+                <div className='badge badge-success'>
+                    Following: {following}
+                </div>
+                <div className='badge badge-light'>
+                    Public Repos: {public_repos}
+                </div>
+                <div className='badge badge-dark'>
+                    Public Gists: {public_gists}
+                </div>
+            </div>
+            <Repos repos={repos} />
+        </Spinner>
+    );
+};
+
+User.propTypes = {
+    getUserDetails: PropTypes.func.isRequired,
+    user: PropTypes.object.isRequired,
+    loading: PropTypes.bool.isRequired,
+    getRepos: PropTypes.func.isRequired,
+    repos: PropTypes.array.isRequired,
+};
 
 const avatarStyle = {
     width: '150px',
